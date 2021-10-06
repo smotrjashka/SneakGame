e448a503a2ef8905b68948537341152bccb51f41
nastiag <nastiag@promptlink.com>
2021-10-06 10:47:11 +0300

    replace procedure for user info in file

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,16 @@
 #include "renderer.h"
 #include <fstream>
 
+const std::string MAIN_FILE = "Sessions.txt";
+const std::string TEMP_FILE = "temp.txt";
+
+std::string save_data_to_file(Game game, std::fstream data_to_write){
+
+    data_to_write << "User:" << game.GetUserName() << " " << "Level:" << game.GetLevel() << " ";
+    data_to_write << "Score:" << game.GetScore() << " " << "Size:" << game.GetSize() << " ";
+    data_to_write << "Lifes:" << game.GetLifes() << "\n";
+}
+
 int main() {
   constexpr std::size_t kFramesPerSecond{30};   //I change started speed to lower
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
@@ -17,17 +27,35 @@ int main() {
   Game game(kGridWidth, kGridHeight);
   game.Run(controller, renderer, kMsPerFrame);
   //saving data for file
-  std::ofstream data_to_write;
-  data_to_write.open("Sessions.txt");
-    if (!data_to_write){
+  std::fstream data_to_write;
+  data_to_write.open(MAIN_FILE, std::ios::out | std::ios::in);
+    if (!data_to_write.is_open()){
         std::count << "problems with write result to file " << std::endl;
     } else {
-        data_to_write << "User:" << game.GetUserName() << "\n";
-        data_to_write << "Level: " << game.GetLevel() << "\n"
-        data_to_write << "Score: " << game.GetScore() << "\n";
-        data_to_write << "Size: " << game.GetSize() << "\n";
-        data_to_write << "Lifes: " << game.GetLifes() << "\n\n";
+        std::string line;
+        std::fstream temp_file;
+        temp_file.open(TEMP_FILE, std::ios::out | std::ios::in);
+        std::string user_name = game.GetUserName();
+        int user_name_length = 5 + user_name.length();
+        bool isPresent = false;
+        while (std::getline(data_to_write, line)){
+            std::cout << "substr: " << line.substr(0, user_name_length) << std::endl;
+            if (line.substr(0, user_name_length) == "User:"+user_name){
+                save_data_to_file(game, temp_file);
+                isPresent = true;
+            } else {
+                temp_file << line << "\n";
+            }
+        }
+        if (!isPresent){
+            save_data_to_file(game, temp_file);
+        }
+
         data_to_write.close();
+        temp_file.close();
+        remove(MAIN_FILE);
+        rename(TEMP_FILE, MAIN_FILE);
+
     }
 
   std::cout << "Game has terminated successfully!\n";
